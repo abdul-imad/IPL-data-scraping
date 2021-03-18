85c5f47572bf54f944a6e5777d0139078dab2674
Abdul Imad <abdimad786@gmail.com>
2021-03-18 22:17:19 +0530

    created and added data to players files

--- allMatches.js
+++ allMatches.js
@@ -5,31 +5,36 @@ let path = require("path");
 function matches(url){
     request(url, cb);
 }
-let arr = [];
 function dirCreator(src){
     if(fs.existsSync(src) == false){
         fs.mkdirSync(src);
     }
 }
-let j = 1;
 let teamName;
 function cb(err, response, html) {
     let chSelector = cheerio.load(html);
     let tables = chSelector(".table.batsman");
     let teams = chSelector(".event .teams .name");
+    let date = chSelector(".match-info.match-info-MATCH .description").text().split(",")[2].trim();
+    let venue = chSelector(".match-info.match-info-MATCH .description").text().split(",")[1].trim();
+    
     for(let i = 0 ; i < 2 ; i++){
         teamName = chSelector(teams[i]).text();
+        if(teamName === "Kings XI Punjab"){
+            teamName = "Punjab Kings";
+        }
         const pathOfFolder = path.join(__dirname,'\\IPL',teamName);
         dirCreator(pathOfFolder);
     }
+    let playerTeam = chSelector(".header-title.label");
     for (let i = 0; i < tables.length; i++) {
-        let teamBowlers = chSelector(tables[i]).find("tr");
-        //console.log(teamBowlers.length);
-        for (let j = 0; j < teamBowlers.length; j++) {
-            let eachbowlcol = chSelector(teamBowlers[j]).find("td");
-            //console.log(eachbowlcol.length);
+        let teamBatsmen = chSelector(tables[i]).find("tr");
+        let eachPlayerTeam = chSelector(playerTeam[i]).text().split("INNINGS")[0].trim() ;
+        for (let j = 0; j < teamBatsmen.length; j++) {
+            let eachbowlcol = chSelector(teamBatsmen[j]).find("td");
             if (eachbowlcol.length == 8) {
-                let playerName = chSelector(eachbowlcol[0]).text();
+                let playerName = chSelector(eachbowlcol[0]).text().trim();
+                playerName=playerName.replace(/ +/g, "");
                 let outDescription = chSelector(eachbowlcol[1]).text();
                 let runs = chSelector(eachbowlcol[2]).text();
                 let balls = chSelector(eachbowlcol[3]).text();
@@ -44,27 +49,42 @@ function cb(err, response, html) {
                     balls : balls,
                     fours : fours,
                     sixes : sixes,
-                    SR : strikeRate
+                    SR : strikeRate,
+                    "match date" : date,
+                    "match venue" : venue,
                 }
-                arr.push(playerObj);
+                
                 let fileName = playerName;
-                let filePath = path.join(__dirname,'\\IPL',);
-                let playerTeam = chSelector(".header-title.label").text();
-                for(let k = 0 ; k < 2 ; k++){
-                    //console.log(playerTeam[k]);
-                }
-                console.log(teamName);
-                
+                let filePath = path.join(__dirname,'\\IPL',eachPlayerTeam , fileName + ".json");
+                if(fs.existsSync(filePath) == false){
+                    fs.openSync(filePath, "a");
+                    let arr1 = [];
+                    arr1.push({
+                        name : playerName,
+                        status : outDescription,
+                        runs : runs,
+                        balls : balls,
+                        fours : fours,
+                        sixes : sixes,
+                        SR : strikeRate,
+                        "match date" : date,
+                        "match venue" : venue,
+                    });
+                    let contentInEmptyFile = JSON.stringify(arr1,null,4);
+                    fs.writeFileSync(filePath, contentInEmptyFile);
 
-                
+                }
+                else{
+                    let contentInFile = fs.readFileSync(filePath);
+                    let arr = JSON.parse(contentInFile);
+                    arr.push(playerObj);
+                    // fs.appendFileSync(filePath, content);   
+                    fs.writeFileSync(filePath, JSON.stringify(arr,null,4));
+                }
             }
         }
-        //console.table(arr);
-        
     }
-
 }
-
 module.exports = {
     matches : matches
 }
